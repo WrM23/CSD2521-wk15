aa5dc5b254e52f8aaf239037446b21625edec758
super <super@AlienOui.lan>
2024-11-23 11:40:36 -0500

    File update 1 - added a comment

---
+++ src/HangmanApp.java
@@ -0,0 +1,27 @@
+/**
+ *
+ * @author Wyatt Metcalf
+ * @date November 19, 2024
+ * @purpose play a game of hangman
+ * 
+ */
+
+import java.util.Scanner;
+
+public class HangmanApp {
+
+    public static void main(String[] args) {
+        System.out.println("Play the H A N G M A N game");
+        
+        Scanner sc = new Scanner(System.in);        
+        String again = "y";        
+        while(again.equalsIgnoreCase("y")) {
+            var hangman = new Hangman();
+            hangman.drawHangman(7);
+            hangman.playGame();
+            System.out.println();
+            System.out.print("Play again? (y/n): ");
+            again = sc.nextLine();
+        }
+    } 
+}
